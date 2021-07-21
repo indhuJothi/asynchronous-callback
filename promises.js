3817feddc1cb8f438dc403528a9b6515b82c0750
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-21 17:05:57 +0530

    commit-1

---
+++ promises.js
@@ -0,0 +1,24 @@
+let done = true
+const IsDone = new Promise((resolve,reject)=>{
+    if(done=false){
+        const workdone= 'The work is done'
+        resolve(workdone)
+    }
+    else{
+        const rej='It is rejected'
+        reject(rej)
+    }
+})
+
+const check =()=>{
+  IsDone.then(ok=>
+  {
+   console.log(ok)
+  }).catch(err=>{
+      console.log(err)
+  }
+
+  )
+}
+
+check()
